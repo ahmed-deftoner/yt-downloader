9007f493fd57454c4b4746f14aa6212d8255e89c
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-10-07 21:14:04 +0500

    video info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use anyhow::Result;
+use qstring::QString;
+use reqwest::{blocking::Client, Url};
+use serde_json::Value;
+
+// read the passed by a user link
+use std::env;
 
 fn get_video_id(url: &str) -> Option<&str> {
     if let Some(id) = regex::Regex::new(r"https://www\.youtube\.com/watch\?v=(.*)")
@@ -10,6 +17,26 @@ fn get_video_id(url: &str) -> Option<&str> {
     }else {
         None
     }
+}
+
+fn get_video_info(url: &str) -> Result<Value> {
+    let json = if let Some(id) = get_video_id(url) {
+        let video_url = format!(
+            "https://www.youtube.com/get_video_info?video_id={}&el=embedded&ps=default",
+            id
+        );
+        let res_body = reqwest::blocking::get(video_url.as_str())?.text()?;
+
+        QString::from(res_body.as_str())
+            .get("player_response")
+            .unwrap_or("")
+            .to_owned()
+    } else {
+        return Err(anyhow::Error::msg("couldn't get video id".to_string()));
+    };
+
+    
+    Ok(serde_json::from_str(&json)?)
 }
 
 #[cfg(test)]
